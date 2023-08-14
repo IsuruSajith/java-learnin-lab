c2ab856e1110893725f93e599bb4b4976c4640be
Isuru <isurusajith0@gmail.com>
2023-08-14 09:50:06 +0530

    research about scanner class,
    input streams before java 1.5

--- src/main/java/isuru/Hello.java
+++ src/main/java/isuru/Hello.java
@@ -1,4 +1,12 @@
 package isuru;
 
+import java.util.Scanner;
+
 public class Hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        System.out.println(input);
+    }
 }
