243a77021338dda0422db86263ed91da461a466b
Isuru <isurusajith0@gmail.com>
2023-08-28 09:48:59 +0530

    call the class and get instance,
    it prints same memory location

---
+++ src/main/java/isuru/singleton/test/Main.java
@@ -0,0 +1,9 @@
+package isuru.singleton.test;
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton1.getInstance().printThis();
+        Singleton1.getInstance().printThis();
+        Singleton1.getInstance().printThis();
+    }
+}
