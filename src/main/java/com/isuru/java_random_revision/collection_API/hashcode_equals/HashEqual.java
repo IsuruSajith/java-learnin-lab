47c08b74c8b50420d6bcf2a714110ca51988305a
Isuru <isurusajith0@gmail.com>
2023-08-28 13:00:11 +0530

    hashcode and equals method should be override to get the proper results
    note and remember it

--- src/main/java/com/isuru/java_random_revision/collection_API/hashcode_equals/HashEqual.java
+++ src/main/java/com/isuru/java_random_revision/collection_API/hashcode_equals/HashEqual.java
@@ -5,6 +5,27 @@ import com.isuru.java_random_revision.collection_API.hashcode_equals.util.Studen
 public class HashEqual {
     public static void main(String[] args) {
 
-        Student student = new Student();
+        Student student = new Student(1,"isuru",55.00);
+        Student student2 = new Student(1,"isuru",55.00);
+
+        System.out.println(student.hashCode()); //-100382254
+        System.out.println(student2.hashCode()); //-100382254
+        System.out.println(student.equals(student2)); //true
+        System.out.println(student==student2); //false
+
+        String name = "isuru";
+        String name2 = "isuru";
+
+        System.out.println(name.equals(name2)); //true
+        System.out.println(name2.hashCode()); //100511758
+        System.out.println(name.hashCode()); //100511758
+        System.out.println(name==name2); // true
+
+        Student student3 = new Student();
+        Student student4 = new Student();
+        System.out.println(student3.equals(student4));//true
+        System.out.println(student3.hashCode());//29791
+        System.out.println(student4.hashCode());//29791
+        System.out.println(student3==student4);//false
     }
 }
--- src/main/java/com/isuru/java_random_revision/collection_API/hashcode_equals/util/Student.java
+++ src/main/java/com/isuru/java_random_revision/collection_API/hashcode_equals/util/Student.java
@@ -1,5 +1,7 @@
 package com.isuru.java_random_revision.collection_API.hashcode_equals.util;
 
+import java.util.Objects;
+
 public class Student {
     int id;
     String name;
@@ -12,5 +14,18 @@ public class Student {
     }
 
     public Student() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && Double.compare(student.weight, weight) == 0 && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, weight);
     }
 }
