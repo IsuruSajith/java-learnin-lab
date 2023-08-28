d998d60f7d30fdd9823dd692b306fe4a9d03aa58
Isuru <isurusajith0@gmail.com>
2023-08-28 11:37:04 +0530

    singleton with holder idiom
    thread safe

---
+++ src/main/java/isuru/singleton/singleton_holder_idiom/Main.java
@@ -0,0 +1,9 @@
+package isuru.singleton.singleton_holder_idiom;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Singleton3.getInstance());
+        System.out.println(Singleton3.getInstance());
+        System.out.println(Singleton3.getInstance());
+    }
+}
