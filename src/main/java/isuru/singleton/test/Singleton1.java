c96c062c51b86d31a1594beb81c42209967f3711
Isuru <isurusajith0@gmail.com>
2023-08-28 09:29:53 +0530

    my sql added to dependency
    going to practice singleton design pattern

---
+++ src/main/java/isuru/singleton/test/Singleton1.java
@@ -0,0 +1,18 @@
+package isuru.singleton.test;
+
+public class Singleton1 {
+    private static Singleton1 singleton1;
+    private String name;
+
+    private Singleton1() {
+        this.name = "isuru";
+    }
+
+    public static Singleton1 getInstance() {
+        return (singleton1 == null) ? singleton1 = new Singleton1() : singleton1;
+    }
+
+    public void printThis() {
+        System.out.println(this);
+    }
+}
