b4bdd261d57f2ebb03cc74555a4a24ca0897f300
Isuru <isurusajith0@gmail.com>
2023-08-30 22:11:23 +0530

    abstraction

---
+++ src/main/java/com/isuru/java_random_revision/abstraction/Example.java
@@ -0,0 +1,9 @@
+package com.isuru.java_random_revision.abstraction;
+
+public abstract class Example {
+    void myMethod();
+    public void myMethod2() {
+        System.out.println("abstract class implemented method");
+    }
+
+}
