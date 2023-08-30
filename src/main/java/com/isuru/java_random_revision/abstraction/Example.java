d6f4899001b7ba9495ae77bb59183f845212d9c8
Isuru <isurusajith0@gmail.com>
2023-08-30 22:11:49 +0530

    corrected method

--- src/main/java/com/isuru/java_random_revision/abstraction/Example.java
+++ src/main/java/com/isuru/java_random_revision/abstraction/Example.java
@@ -1,7 +1,7 @@
 package com.isuru.java_random_revision.abstraction;
 
 public abstract class Example {
-    void myMethod();
+    abstract void myMethod();
     public void myMethod2() {
         System.out.println("abstract class implemented method");
     }
