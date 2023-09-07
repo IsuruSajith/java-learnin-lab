cb21e03ce299398b319796f2236aa30e63918f19
Isuru <isurusajith0@gmail.com>
2023-09-07 11:28:31 +0530

    leet code second q

--- src/main/java/com/isuru/leetcode/Solution.java
+++ src/main/java/com/isuru/leetcode/Solution.java
@@ -18,9 +18,44 @@ public class Solution {
         return null;
     }
 
+
+    public static int lengthOfLongestSubstring(String s) {
+        int count = 0;
+        char[] chars = s.toCharArray();
+        System.out.println(Arrays.toString(chars));
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = i+1; j < chars.length; j++) {
+                if (chars[i]==chars[j]) {
+                    chars[j]=0;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(chars));
+        for (char aChar : chars) {
+            if (aChar!=0) {
+                count++;
+            }
+        }
+
+
+        return count;
+    }
+
+
+
+
+
+
+
     public static void main(String[] args) {
-        int[] nums = new int[]{2,7,11,15};
+        /*int[] nums = new int[]{2,7,11,15};
         int[] ints = twoSum(nums, 9);
-        System.out.println(Arrays.toString(ints));
+        System.out.println(Arrays.toString(ints));*/
+
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("bbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+
     }
 }
