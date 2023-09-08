b0a7d33ee18fb276c226406eb3ca39708c01a321
Isuru <isurusajith0@gmail.com>
2023-09-08 21:31:09 +0530

    leet code bad version problem

--- src/main/java/com/isuru/leetcode/Solution.java
+++ src/main/java/com/isuru/leetcode/Solution.java
@@ -100,6 +100,35 @@ public class Solution {
         return 0;
     }
 
+    //---------------------------------------------//
+        public  int firstBadVersion(int n) {
+            int left = 1;
+            int right = n;
+
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+
+                if (isBadVersion(mid)) {
+                    // If mid is a bad version, move the search to the left half
+                    right = mid;
+                } else {
+                    // If mid is not a bad version, move the search to the right half
+                    left = mid + 1;
+                }
+            }
+
+            // The loop ends when left == right, representing the first bad version
+            return left;
+        }
+
+        // This is a placeholder for the isBadVersion method provided by the API
+        private boolean isBadVersion(int version) {
+            // Implement this method according to your API
+            // For this example, let's assume it's implemented elsewhere
+            return true; // Replace with actual implementation
+        }
+
+
 
 
     public static void main(String[] args) {
