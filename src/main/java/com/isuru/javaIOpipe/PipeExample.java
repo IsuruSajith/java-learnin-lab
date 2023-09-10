567259971d7c4806db82fbe2f127eeef49560d31
Isuru <isurusajith0@gmail.com>
2023-09-10 19:19:42 +0530

    create streams

--- src/main/java/com/isuru/javaIOpipe/PipeExample.java
+++ src/main/java/com/isuru/javaIOpipe/PipeExample.java
@@ -1,7 +1,12 @@
 package com.isuru.javaIOpipe;
 
-public class PipeExample {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 
+public class PipeExample {
+    public static void main(String[] args) throws IOException {
+        PipedOutputStream pos = new PipedOutputStream();
+        PipedInputStream pis = new PipedInputStream(pos);
     }
 }
