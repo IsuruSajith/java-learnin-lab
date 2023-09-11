cdff150820c849c4362b8b9ac28614134b6e71b5
Isuru <isurusajith0@gmail.com>
2023-09-11 20:58:12 +0530

    create a node class for linked list

--- src/main/java/com/isuru/leetcode/ListNode.java
+++ src/main/java/com/isuru/leetcode/ListNode.java
@@ -2,4 +2,19 @@ package com.isuru.leetcode;
 
 public class ListNode {
 
+    int val;
+    ListNode next;
+
+    public ListNode() {
+
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
