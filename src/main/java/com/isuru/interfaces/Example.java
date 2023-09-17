83a7ad1086b00d12a4d16cab407cadf5cf3e93dc
Isuru <isurusajith0@gmail.com>
2023-09-17 17:36:29 +0530

    The purpose of default interface methods is to allow the addition of new methods to existing interfaces without breaking the implementations of those interfaces. It provides a way to extend interfaces in a backward-compatible manner.

--- src/main/java/com/isuru/interfaces/Example.java
+++ src/main/java/com/isuru/interfaces/Example.java
@@ -1,4 +1,8 @@
 package com.isuru.interfaces;
 
 public class Example implements MyInterface {
+    @Override
+    public void myMethod() {
+        System.out.println("override method in interface");
+    }
 }
--- src/main/java/com/isuru/interfaces/MyInterface.java
+++ src/main/java/com/isuru/interfaces/MyInterface.java
@@ -1,5 +1,9 @@
 package com.isuru.interfaces;
 
 public interface MyInterface {
+    void myMethod();
 
+    default void myDefaultMethod() {
+        System.out.println("default method in java");
+    }
 }
