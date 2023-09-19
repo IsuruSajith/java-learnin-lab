0a701edc6865eaf891c77e602ea1b54d5e05ff3b
Isuru <isurusajith0@gmail.com>
2023-09-19 21:56:40 +0530

    create non abstract method in abstract class

--- src/main/java/com/isuru/abstraction/Shape.java
+++ src/main/java/com/isuru/abstraction/Shape.java
@@ -3,4 +3,7 @@ package com.isuru.abstraction;
 public abstract class Shape {
     protected int x, y;
     public abstract double calculateArea();
+    public void display() {
+        System.out.println("Shape at (" + x + ", " + y + ")");
+    }
 }
