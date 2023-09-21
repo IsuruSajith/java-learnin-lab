7c9d4c68c729f24b2262b2ede92ea010f69f95fc
Isuru <isurusajith0@gmail.com>
2023-09-21 20:32:39 +0530

    complete the count class

--- src/main/java/com/isuru/thread/Counter.java
+++ src/main/java/com/isuru/thread/Counter.java
@@ -1,5 +1,14 @@
 package com.isuru.thread;
 
 public class Counter {
+    private int count = 0;
 
+    public synchronized void increment() {
+        count++;
+        System.out.println("Incremented: " + count);
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
