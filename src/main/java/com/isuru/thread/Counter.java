b8936f05d62deb00786f895db7aaf54fe2d4f070
Isuru <isurusajith0@gmail.com>
2023-09-21 20:33:43 +0530

    creat IncrementTask class

--- src/main/java/com/isuru/thread/Counter.java
+++ src/main/java/com/isuru/thread/Counter.java
@@ -12,3 +12,24 @@ public class Counter {
         return count;
     }
 }
+
+class IncrementTask implements Runnable {
+    private Counter counter;
+
+    public IncrementTask(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            counter.increment();
+            try {
+                Thread.sleep(500); // Simulate some work
+            } catch (InterruptedException e) {
+                System.err.println("Thread interrupted.");
+            }
+        }
+    }
+}
+
