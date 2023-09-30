9d87e98352a478d9b4f243454f44907480fb45e7
Isuru <isurusajith0@gmail.com>
2023-09-30 19:23:32 +0530

    implement the factory design pattern

--- src/main/java/com/isuru/desing_patterns/FactoryMethodExample.java
+++ src/main/java/com/isuru/desing_patterns/FactoryMethodExample.java
@@ -33,5 +33,13 @@ class ConcreteCreatorB extends Creator {
     }
 }
 public class FactoryMethodExample {
+    public static void main(String[] args) {
+        Creator creatorA = new ConcreteCreatorA();
+        Product productA = creatorA.factoryMethod();
+        productA.create();
 
+        Creator creatorB = new ConcreteCreatorB();
+        Product productB = creatorB.factoryMethod();
+        productB.create();
+    }
 }
