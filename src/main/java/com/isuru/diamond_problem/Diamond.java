bf48430e396c0f62b3c76a2efbaa795f178743be
Isuru <isurusajith0@gmail.com>
2023-12-26 12:10:20 +0530

    diamond problem in inheritance

--- src/main/java/com/isuru/diamond_problem/Diamond.java
+++ src/main/java/com/isuru/diamond_problem/Diamond.java
@@ -2,6 +2,35 @@ package com.isuru.diamond_problem;
 
 public class Diamond {
     public static void main(String[] args) {
-
+        D d = new D();
+        d.display();  // Output: Class B
     }
 }
+
+
+interface A {
+    void display();
+}
+
+class B implements A {
+    public void display() {
+        System.out.println("Class B");
+    }
+}
+
+class C implements A {
+    public void display() {
+        System.out.println("Class C");
+    }
+}
+
+class D extends B {
+    public void display() {
+        // Specify which interface's method you want to call
+        //B.super.display(); // Call display method from interface B
+    }
+}
+
+
+
+
