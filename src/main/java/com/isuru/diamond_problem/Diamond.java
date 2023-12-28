1a06388b9728fb8cb8ae63fe173d86234e8ea9e6
Isuru <isurusajith0@gmail.com>
2023-12-28 12:10:20 +0530

    diamond problem in inheritance

--- src/main/java/com/isuru/diamond_problem/Diamond.java
+++ src/main/java/com/isuru/diamond_problem/Diamond.java
@@ -2,16 +2,15 @@ package com.isuru.diamond_problem;
 
 public class Diamond {
     public static void main(String[] args) {
-        // Creating an anonymous class implementing Interface D
+
         D d = new D() {
-            // Implementing display() is mandatory due to diamond problem
+
             @Override
             public void display() {
                 System.out.println("Implementation in Interface D");
             }
         };
 
-        d.display();  // Output: Implementation in Interface D
     }
 }
 
