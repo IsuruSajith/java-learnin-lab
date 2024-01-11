a58276e307981e57d66a908cf605c4a3fa7de483
Isuru <isurusajith0@gmail.com>
2024-01-11 08:21:35 +0530

    tree map

--- src/main/java/com/isuru/java_interview_q/hashmap_treemap/Example.java
+++ src/main/java/com/isuru/java_interview_q/hashmap_treemap/Example.java
@@ -2,6 +2,7 @@ package com.isuru.java_interview_q.hashmap_treemap;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Example {
     // difference between hashmap and treemap
@@ -11,7 +12,17 @@ public class Example {
         hashMap.put("isuru", 13231);
         hashMap.put("sajith", 13231);
         hashMap.put("isuru", 13230);
+        hashMap.put(null, 13230);
+
         System.out.println(hashMap);
+
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+        treeMap.put(1, "isuru");
+        treeMap.put(1, "sajith");
+        treeMap.put(2, "sajith");
+       // treeMap.put(null, "isuru");
+        System.out.println(treeMap);
+
 
     }
 
