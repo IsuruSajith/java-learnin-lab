d28e16c34e6d4aae0d881c854f35db1cba7ed4c1
Isuru <isurusajith0@gmail.com>
2024-01-11 08:53:38 +0530

    @FunctionalInterface interface MyFunctionalInterface {     void myMethod(); }

---
+++ src/main/java/com/isuru/java_interview_q/hashmap_treemap/LambdaExample.java
@@ -0,0 +1,12 @@
+package com.isuru.java_interview_q.hashmap_treemap;
+
+public class LambdaExample {
+        public static void main(String[] args) {
+            MyFunctionalInterface myLambda = ()->{};
+
+        }
+}
+@FunctionalInterface
+interface MyFunctionalInterface {
+    void myMethod();
+}
