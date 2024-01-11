822e70c10aa29d2adf13cb94651f12ed369efb6e
Isuru <isurusajith0@gmail.com>
2024-01-11 08:56:21 +0530

    working lambda expression

--- src/main/java/com/isuru/java_interview_q/hashmap_treemap/LambdaExample.java
+++ src/main/java/com/isuru/java_interview_q/hashmap_treemap/LambdaExample.java
@@ -1,9 +1,10 @@
 package com.isuru.java_interview_q.hashmap_treemap;
-
 public class LambdaExample {
         public static void main(String[] args) {
-            MyFunctionalInterface myLambda = ()->{};
-
+            MyFunctionalInterface myLambda = ()->{
+                System.out.println("ajith hermanos");
+            };
+            myLambda.myMethod();
         }
 }
 @FunctionalInterface
